53f89f5f565827d519a317503cac72d11746b8f3
luzhihong <xiaopikaqiu@outlook.com>
2025-05-20 20:37:25 +0800

    Added images for testing

--- src/app/blog/seconde/page.tsx
+++ src/app/blog/seconde/page.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
 export default function Seconde() {
-  return <h1>Seconde blog</h1>;
+  return (
+    <>
+      <h1>Seconde blog</h1>
+      <Image src="/sea.jpg" alt="sea" width={100} height={100} />
+    </>
+  );
 }
